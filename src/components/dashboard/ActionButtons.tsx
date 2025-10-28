f4a41d60c806945982029d6dd61cae7c5727da7f
Michał Górniak <michag800@gmail.com>
2025-10-28 14:11:34 +0100

    WIP: Add Mount Fuji to the dashboard

--- src/components/dashboard/ActionButtons.tsx
+++ src/components/dashboard/ActionButtons.tsx
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
     elevation: 8,
   },
   buttonOutline: {
-    backgroundColor: 'transparent',
+    backgroundColor: "#fff",
     borderWidth: 2,
     borderColor: colors.primary,
   },
